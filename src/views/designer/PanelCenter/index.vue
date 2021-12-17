2c64ec5658d10e636e7c28f5a30d4e9683099e38
huangwenxia <892027354@qq.com>
2021-12-17 18:13:39 +0800

    Panel开发

--- src/views/designer/PanelCenter/index.vue
+++ src/views/designer/PanelCenter/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="panel-center">
-中间
+    <div class="wrapper">
+      <div class="head">
+        <div class="title">{{'测试表达' }}</div>
+        <div class="desc">{{ '测试表达' }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,5 +24,26 @@
   min-height: calc(100vh - 50px);
   background: #f1f5f1;
   padding: 10px 0;
+  .wrapper{
+    background: rgba(255, 255, 255, 0.9);
+    min-height: calc(100vh - 70px);
+    width: 800px;
+    margin: 0 auto;
+    box-shadow: 0 2px 5px 1px rgba(0, 0, 0, 0.05);
+    .head {
+      padding: 20px;
+      text-align: center;
+      border-bottom: 1px solid #ddd;
+      .title {
+        font-size: 24px;
+        font-weight: 700;
+        line-height: 1.4;
+      }
+      .desc {
+        margin-top: 10px;
+      }
+    }
+  }
 }
+
 </style>
---
+++ src/views/designer/PanelLeft/Modules.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="Modules">
+      <div class="m-box" v-for="(item,index) in modules" :key="index">
+        <div class="head">{{item.name}}</div>
+        <ul class="list">
+          <li  class="item" v-for="(child,i) in item.children" :key="i" @mousedown.stop="onMouseDown" :data-info="child.name + '|' + child.icon + '|' + child.type">
+            <component class="icon" :is="child.icon" />
+            <span class="name">{{ child.name }}</span>
+          </li>
+        </ul>
+        <div class="content">
+
+        </div>
+      </div>
+    </div>
+
+</template>
+<script lang="ts">
+import { PicCenterOutlined, UnorderedListOutlined, DownSquareOutlined, CalendarOutlined, FieldBinaryOutlined } from "@ant-design/icons-vue"
+export default {
+  components: { PicCenterOutlined, UnorderedListOutlined, DownSquareOutlined, CalendarOutlined, FieldBinaryOutlined },
+
+}
+</script>
+<script setup lang="ts">
+    import {reactive} from "vue";
+    const modules = reactive( [
+      {
+        name: "常用",
+        children: [
+          { name: "文本框", icon: "PicCenterOutlined", type: "input" },
+          { name: "单选", icon: "UnorderedListOutlined", type: "radio" },
+          { name: "多选", icon: "UnorderedListOutlined", type: "checkbox" },
+          { name: "下拉", icon: "DownSquareOutlined", type: "select" },
+          { name: "时间日期", icon: "CalendarOutlined", type: "date" },
+          { name: "数字", icon: "FieldBinaryOutlined", type: "number" }
+        ]
+      }
+    ])
+</script>
+
+<style scoped lang="scss">
+.Modules{
+  .m-box{
+    margin-bottom: 10px;
+    .head {
+      margin-bottom: 10px;
+      i {
+        color: #999;
+      }
+    }
+    .list{
+      margin: 0;padding: 0;
+      .item{
+        width: 70px;
+        display: inline-block;
+        text-align: center;
+        color: #666;
+        padding: 5px 0;
+        cursor: pointer;
+        border-radius: 3px;
+        font-size: 12px;
+        margin-bottom: 10px;
+        .icon{
+          line-height: 1;
+          vertical-align: top;
+          pointer-events: none;
+          width: 1em;
+          height: 1em;
+          display: inline-block;
+          font-size: 30px;
+          color: #999;
+          margin-bottom: 5px;
+        }
+        .name{//变成块元素就换行了
+          display: block;
+          pointer-events: none;
+        }
+        &:hover {
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+      }
+    }
+
+  }
+}
+</style>
