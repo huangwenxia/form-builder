240f48c08b189c7076e4dfa1c15357e7fb07d5e3
huangwenxia <892027354@qq.com>
2021-12-28 17:32:47 +0800

    fda

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue"
+import App from "./App.vue"
 import router from "./router"
 import store from "./store"
 import comps from "./components/global"
@@ -7,11 +7,11 @@ import "normalize.css"
 // import "./assets/css/main.scss" hwx:在viteConfig中配置了
 import Antd from "ant-design-vue"
 import "ant-design-vue/dist/antd.css"
+
 const app = createApp(App)
 comps.forEach((a) => {
-        const path  = a.__file || "";
-        path.match(/(\w+)?\.(\w+)/);
-        app.component(RegExp.$1, a)
+    const path = a.__file || ""
+    path.match(/(\w+)?\.(\w+)/)
+    app.component(RegExp.$1, a)
 })
-app.use(store).use(router).use(Antd).mount('#app')
-
+app.use(store).use(router).use(Antd).mount("#app")
